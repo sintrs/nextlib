a56fe5e5eb49e6d40cf5b1eafb2f72bfae987f34
sintrs <bleachi@qq.com>
2025-06-11 13:31:32 +0800

    Update setup.sh

--- ffmpeg/setup.sh
+++ ffmpeg/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Versions
-VPX_VERSION=1.13.0
-MBEDTLS_VERSION=3.4.1
-FFMPEG_VERSION=6.0
+VPX_VERSION=1.14.1
+MBEDTLS_VERSION=3.6.3
+FFMPEG_VERSION=6.1.2
 
 # Directories
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
